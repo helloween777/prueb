026cb17f5c8fe0f2bd1af3222541cc93d91030d0
helloween777 <i2422394@continental.edu.pe>
2024-10-14 21:44:35 -0500

    creando ejercicio 2 y 3

--- sem06.py
+++ sem06.py
@@ -8,3 +8,14 @@ st.subheader("Ejercicio 1: imprimir 'Hola Mundo' 10 veces")
 if st.button("Ejecutar Ejercicio 1"):
     for i in range(10):
         st.write("Hola Mundo")
+#Ejercicio 2: Imprimir los primeros 10 números
+st.subheader("Ejercicio 2: Imprimir los primeros 10 números")
+if st.button("Ejecutar Ejercicio 2"):
+    for i in range(1, 11):
+        st.write(i)
+#Ejercicio 3: tabla de multiplicar
+st.subheader("Ejercicio 3: Imprimir la tabla de multiplicar del número ingresado")
+num = st.number_input("Ingrese un número para ver su tabla de multuplicar del 1 al 12", min value=1)
+if st.button("Ejecutar Ejercicio 3"):
+    for in range(1, 13):
+        st.write(f"{num} x {i} = {num * i}")
