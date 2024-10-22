82224fd0860f2054b6127c9986bdbd7f31f66118
helloween777 <i2422394@continental.edu.pe>
2024-10-21 21:29:51 -0500

    crear prog

---
+++ Sem07.py
@@ -0,0 +1,23 @@
+import streamlit as streamlit
+
+def mostrar menu():
+    st.title("Ejemplo de Menú")
+    st.write("Selecciona una opción de menú")
+
+    menu = ["Archivo", "Editar", "Ver", "Salir"]
+    seleccion = ""
+
+    
+        seleccion = st.radio("Menú", menu)
+
+        if seleccion == "Archivo":
+            st.write("Seleccionaste: Archivo")
+        elif seleccion == "Editar":
+            st.write("Seleccionaste: Editar")
+        elif seleccion == "ver":
+            st.write("Seleccionaste: Ver")
+        elif seleccion == "Salir":
+            st:write("¡Saliendo del menú!")
+
+if __name__=="__main__":
+    mostrar_menu()
